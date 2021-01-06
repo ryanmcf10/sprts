4d483303fa3e59b137ff101fcfdc05a3fccf30f9
Ryan McFarland <ryanmcf10@gmail.com>
2021-01-06 14:51:17 -0500

    Remove async

--- sprts/nba/management/commands/update_rosters.py
+++ sprts/nba/management/commands/update_rosters.py
@@ -1,4 +1,4 @@
-import json, asyncio, time
+import json, time
 from datetime import date
 
 from django.core.management.base import BaseCommand
@@ -10,37 +10,39 @@ from nba.models import NBAPlayer, NBATeam
 
 class Command(BaseCommand):
     help = 'Fetch the latest Team Rosters from NBA API and update'
-    
+
     def handle(self, *args, **options):
         for team in NBATeam.objects.all():
             print(f"{time.strftime('%X')}: {team}")
-            asyncio.run(self.update_roster_with_delay(team))
+            self.update_roster(team)
+            time.sleep(2)
 
-
-    async def update_roster_with_delay(self, team, delay=2):
-        """
-        Update the rosters for the given team, in at most 2 seconds.
-
-        Built-in delay prevents requests to the API from being blocked.
-        :param team: NBATeam
-        :param delay: minimum number of seconds method should take to complete
-        :return:
-        """
-        await self.update_roster(team)
-        await asyncio.sleep(delay)
-
-    async def update_roster(self, team):
+    def update_roster(self, team):
         json_roster = self.fetch_json_roster(team)
         roster_data = self.parse_json_roster(json_roster)
 
         players = roster_data['CommonTeamRoster']
+        self.update_players(players)
 
-        for player in players:
-            print(player['PLAYER'])
-        #for k, v in player.items():
-        #    print(k, v)
+    def update_players(self, players):
+        for raw_player in players:
+            nba_api_id = raw_player['PLAYER_ID']
+
+            try:
+                player = NBAPlayer.objects.get(nba_api_id=nba_api_id)
+                print(f"Found {player}")
+
+            except NBAPlayer.DoesNotExist:
+                print(f"Could not find {raw_player['PLAYER']}")
+                player = NBAPlayer(nba_api_id=nba_api_id)
+                player.fetch_player_info()
 
     def fetch_json_roster(self, team):
+        """
+        Request today's roster using NBA API and return it as 'messy' JSON.
+        :param team: NBATeam
+        :return:
+        """
         raw_roster = commonteamroster.CommonTeamRoster(team.nba_api_id)
 
         json_roster = json.loads(raw_roster.nba_response.get_json())
@@ -50,7 +52,8 @@ class Command(BaseCommand):
 
     def parse_json_roster(self, json_roster):
         """
-        crazy comprehensions to get a dictionary like:
+        Clean up the 'messy' JSON returned from NBA API using some crazy list/dict comprehensions to get something more
+        usable like:
 
         {
             'CommonTeamRoster': [
@@ -58,7 +61,7 @@ class Command(BaseCommand):
                 { player_2 dictionary},
                 ...
                 { player_n dictionary},
-            
+
             'Coaches': [
                 { coach_1 dictionary},
                 { coach_2 dictionary},
@@ -69,8 +72,18 @@ class Command(BaseCommand):
         """
 
         return {
-            x['name']: [{
-                    x['headers'][i]: player[i] for i in range(len(x['headers']))
-                } for player in x['rowSet']
-            ] for x in json_roster['resultSets']
-        } 
+                    x['name']: [{
+                            x['headers'][i]: player[i] for i in range(len(x['headers']))
+                        } for player in x['rowSet']
+                    ] for x in json_roster['resultSets']
+                }
+
+    def parse_player(self, player_dict):
+        nba_api_id = player_dict['PLAYER_ID']
+        team_nba_api_id = player_dict['TEAM_ID']
+        name = player_dict['PLAYER']
+        number = player_dict['NUM']
+        position = player_dict['POSITION']
+        height = player_dict['HEIGHT']
+        weight = player_dict['WEIGHT']
+        birth_date = player_dict['BIRTH_DATE']
