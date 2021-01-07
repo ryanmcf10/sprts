d5ac3bbd19995298593cf98c2a7936fe055e7270
Ryan McFarland <ryanmcf10@gmail.com>
2021-01-07 14:15:18 -0500

    Add method to get a team's current players

--- sprts/nba/models/nbateam.py
+++ sprts/nba/models/nbateam.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.apps import apps
 
 from shared.models import BaseTeam
 
@@ -9,3 +10,13 @@ class NBATeam(BaseTeam):
     year_founded = models.TextField(blank=True)
 
     nba_api_id = models.CharField(max_length=12, unique=True, editable=False)
+
+    def get_current_player_memberships(self):
+        return self.player_memberships.filter(end_date=None)
+
+    def get_current_players(self):
+        NBAPlayer = apps.get_model('nba', 'NBAPlayer')
+
+        return NBAPlayer.objects.filter(
+            team_membership__in=self.get_current_player_memberships()
+        )
